df319aff5527ccb2ea2605a9204ab9c53a971e58
tumit <tumit@live.com>
2019-02-07 18:06:22 +0700

    rename selector

--- projects/ngq-datepicker/src/lib/ngq-datepicker.component.ts
+++ projects/ngq-datepicker/src/lib/ngq-datepicker.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'lib-ngq-datepicker',
+  selector: 'ngq-datepicker',
   template: `
     <p>
       ngq-datepicker works!
